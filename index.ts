572c888ae1c6ec249a7707f62bda6fcbf3e8613c
griminir <viktor.elliot.king@gmail.com>
2024-03-24 21:59:48 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // annotation
 //strings
-let myName: string = 'viktor';
+let myName: string = 'Viktor';
 
 //number
 let myAge: number = 29;
@@ -121,3 +121,91 @@ const larry: fullInfo = {
   password: 'timmy84',
 };
 console.log(larry);
+
+//unions, one or several possible types
+//can be all types of annotations
+let union: string | number;
+union = 1;
+union = 'hello';
+
+//look back at arrays to understand the second annotation
+//can be all types of annotations
+function unionF(param: string | string[]) {
+  //function body
+}
+
+//this also works with intersection types
+let kåre: UserInfo | accountDetails = {
+  email: 'string@gmail.com',
+  password: 'hello',
+};
+
+//also works with arrays
+const stuff: (number | string)[] = [1, 1, 1, 3, 'hello'];
+console.log(stuff);
+
+//literal types same syntax for number and boolean
+let color: 'red' | 'green' | 'blue';
+color = 'red';
+
+//tuples are an array with a fixed number of elements where each element can hava a different type
+let myTuple: [string, number, number] = ['hello', 1, 32];
+console.log(myTuple);
+
+//destructuring
+let [first, number2, third] = myTuple;
+console.log(first);
+console.log(number2);
+console.log(third);
+
+//enums
+enum WeatherConditions {
+  Sunny = 'sunny',
+  Cloudy = 'cloudy',
+  Rainy = 'rainy',
+}
+
+const currentWeather = WeatherConditions.Rainy;
+console.log(`the current weather is ${currentWeather}`);
+
+//interface how to make a object have a stricter structure
+//interfaces can also be used to describe the structure of functions and classes as well
+//p.s classes belong to object orianted programming
+//readonly and ? work the same as making a type
+// you can add to an interface by typing it again and between the {owner: string} and it will be added to the interface rules
+interface Computor {
+  readonly name: string;
+  ram: number;
+  hdd?: number;
+}
+
+const computorExample: Computor = {
+  name: 'i7',
+  ram: 18,
+  hdd: 512,
+};
+
+//function interface
+interface MathOperations {
+  (x: number, y: number): number;
+}
+
+const add: MathOperations = (a, b) => a + b;
+const subtract: MathOperations = (a, b) => a - b;
+console.log(add(2, 8));
+console.log(subtract(500, 10));
+
+//generics type or t is a Type perimiter area often you just use a T
+//you basically say whatever i put in between <> will be the type i want, so later when you are using the function you can but the annotation you need and it will make all of them named the same the same annotation
+function uniqueDataTypesFunc<Type>(
+  item: Type,
+  defaultValue: Type
+): [Type, Type] {
+  return [item, defaultValue];
+}
+
+//in this example since we say number we need to use numbers and we will return numbers
+const res5 = uniqueDataTypesFunc<number>(10, 20);
+console.log(res5);
+
+//type narrowing
